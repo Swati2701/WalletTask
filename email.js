f6fbae6ea4b7cd326febf4be9121d9e7791f3d1a
Swati Jindal <88382946+Swati2701@users.noreply.github.com>
2021-10-08 12:01:32 +0530

    Updated file

--- email.js
+++ email.js
@@ -2,32 +2,30 @@
 const nodemailer = require('nodemailer');
 require('dotenv').config({ path: './config.env' });
 
-async function sendEmail(email, token) {
+async function sendEmail(email, token, code) {
     try{
-        //const smtpEndPoint = 'smtp.sendgrid.net';
-        //const port = 465;
         const senderAddress = 'test@gmail.com';
         let toAddress = email;
-        const username = process.env.SENDGRID_USERNAME;
-        const password = process.env.SENDGRID_PASSWORD;
         let subject = "verify your email";
 
         let html = `<!DOCTYPE>
         <html>
         <body>
-        <p> your code is : </p> <b> ${token} </b>
+        <p>Please verify your email address:</p> <b> ${token} </b>
+        <p>Please use this code while login: </p> <b> ${code} </b>
         </body>
         </html>`;
 
-        let transport = nodemailer.createTransport({
-            service: 'SendGrid',
-            auth:{
-                user:username,
-                pass: password
-            }
-        })
+        var transport = nodemailer.createTransport({
+            host: "smtp.mailtrap.io",
+            port: 2525,
+            auth: {
+                user: "e6ff4e3edf8984",
+                pass: "d11c3450387fe5"
+  }
+          });
 
-        let mailOptions ={
+        let mailOptions = {
             from: senderAddress,
             to: toAddress,
             subject: subject,
@@ -38,7 +36,7 @@ async function sendEmail(email, token) {
         return { error: false };
 
     } catch (error){
-        console.log("send email error");
+        console.log("send email error", error);
         return{
             error: true,
             message: "cannot send email"
@@ -46,4 +44,47 @@ async function sendEmail(email, token) {
     }
 }
 
-module.exports = { sendEmail };
+
+
+async function sendTransactionDetails(TransactionsDetails, email) {
+    try{
+        const senderAddress = 'noreply@gmail.com';
+        let toAddress = email;
+        let subject = "Transaction Details";
+        console.log(toAddress);
+        let html = `<!DOCTYPE>
+        <html>
+        <body>
+        <p>Succesfully transfer!! please check transaction details:</p> <b> ${TransactionsDetails} </b>
+        </body>
+        </html>`;
+
+        var transport = nodemailer.createTransport({
+            host: "smtp.mailtrap.io",
+            port: 2525,
+            auth: {
+                user: "e6ff4e3edf8984",
+                pass: "d11c3450387fe5"
+  }
+          });
+
+        let mailOptions = {
+            from: senderAddress,
+            to: toAddress,
+            subject: subject,
+            html: html
+        };
+
+        let info = await transport.sendMail(mailOptions);
+        return { error: false };
+
+    } catch (error){
+        console.log("send email error", error);
+        return{
+            error: true,
+            message: "cannot send email"
+        }
+    }
+}
+
+module.exports = { sendEmail, sendTransactionDetails };
--- server.js
+++ server.js
@@ -7,11 +7,12 @@ const cookieParser = require('cookie-parser');
 require('dotenv').config({ path: './config.env' });
 
 const userRouter = require('./Routes/userRouter');
-
+const transactionRouter = require('./Routes/transactionRouter');
 app.use(express.json());
 app.use(cookieParser());
-app.use('/api', userRouter);
 
+app.use('/api', userRouter);
+app.use('/api', transactionRouter);
 const port = 8000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
