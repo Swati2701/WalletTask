de973042e2db8fe6dbab54239ac57bb84bcceb26
Swati Jindal <88382946+Swati2701@users.noreply.github.com>
2021-10-08 17:25:05 +0530

    Updated files

--- Routes/userRouter.js
+++ Routes/userRouter.js
@@ -2,12 +2,12 @@
 
 const express = require('express');
 const userController = require('./../controller/userController');
-
+const authController = require('./../controller/authController');
 const router = express.Router();
 
 
 router.post('/signup', userController.signup); 
 router.post('/login', userController.login);
 router.get('/signout', userController.signOut);
-
+router.get('/welcome', authController, userController.welcome);
 module.exports = router;
