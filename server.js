b6fb308f80d8236a5e56ec01454d1372a8b42c25
Swati Jindal <88382946+Swati2701@users.noreply.github.com>
2021-10-08 14:13:04 +0530

    Updated files

--- server.js
+++ server.js
@@ -1,18 +1,9 @@
 /* eslint-disable */
-//const dotenv = require('dotenv');
-const express = require('express');
 const mongoose = require('mongoose');
-const app = express();
-const cookieParser = require('cookie-parser');
 require('dotenv').config({ path: './config.env' });
 
-const userRouter = require('./Routes/userRouter');
-const transactionRouter = require('./Routes/transactionRouter');
-app.use(express.json());
-app.use(cookieParser());
+const app = require('./app');
 
-app.use('/api', userRouter);
-app.use('/api', transactionRouter);
 const port = 8000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
